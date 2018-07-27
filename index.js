30344feee828af574e604d4dd950d249723087d6
xcc99 <39719446+xcc99@users.noreply.github.com>
2018-07-27 18:02:04 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+var server = http.createServer(function(req,res){
+  var pathObj = url.parse(req.url,true)
+  switch(pathObj.parsename){
+    case '/getWeather' :
+    var ret
+    if(pathObj.query.city === 'beijing'){
+      ret{
+        city: 'beijing',
+        weather: '阴天'
+      }
+    }else{
+      ret{
+        city: pathObj.query.city,
+        weather: '未知'
+      }
+    }
+    res.end(JOSON.strinfify(ret))
+    break;
+    case '/introduce':
+    res.end(fs.readFlieSync(__dirname + '/README.md'))
+  }
+  break;
+  default:
+  res.end(fs.readFlieSync(__dirname + '/sample' + pathObj.pathname))
+  
+})
